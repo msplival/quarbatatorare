40d38fd3f79ff6dc3e8eafb5aa0b736f35b066f7
Mario Splivalo <mario@splivalo.hr>
2022-04-25 17:49:26 +0200

    Formatting...

--- gitter.sh
+++ gitter.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ]
 do
-	echo "Updated on $(date)" >> README.md
+	echo -e "\nUpdated on $(date)\n" >> README.md
 	git commit README.md -m "$(curl -s whatthecommit.com/index.txt)"
 	git push
 	sleep 1d
