ff5c10552e010392cb64a73518c556c6bcfe1e53
Mario Splivalo <mario@splivalo.hr>
2024-05-29 23:07:18 +0200

    We had to have this one changed... :D

--- gitter.sh
+++ gitter.sh
@@ -3,7 +3,7 @@
 while [ 1 ]
 do
 	echo -e "\nUpdated on $(date)\n" >> README.md
-	git commit README.md -m "$(curl -s whatthecommit.com/index.txt)"
+	git commit README.md -m "$(curl -s https://whatthecommit.com/index.txt)"
 	git push
 	sleep ${RANDOM}m
 done
